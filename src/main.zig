3a26534975a6c60ee9a16131fb7b4a41cfe3ab85
山下 <hello@yamashit.ax>
2022-10-23 12:32:23 +0900

    First working version

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,56 @@
 const std = @import("std");
 
+const target_directories = [_][]const u8{ "Projects", "Sites", "Code" };
+const delete_directories = [_][]const u8{ "node_modules", "vendor" };
+
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const allocator = arena.allocator();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const home_dir = std.os.getenv("HOME") orelse {
+        return error.HomeDirNotAvailable;
+    };
 
-    try bw.flush(); // don't forget to flush!
-}
+    for (target_directories) |target| {
+        const path = try std.fs.path.join(allocator, &[_][]const u8{ home_dir, target });
+        defer allocator.free(path);
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+        const dir = std.fs.cwd().openIterableDir(path, .{}) catch continue;
+        var walker = try dir.walk(allocator);
+        defer walker.deinit();
+
+        while (try walker.next()) |entry| {
+            if (entry.kind != .Directory) continue;
+            if (std.mem.startsWith(u8, entry.path, ".")) continue;
+
+            for (delete_directories) |delete| {
+                if (std.mem.endsWith(u8, entry.path, delete)) {
+                    const parent_dir = std.fs.path.dirname(entry.path) orelse continue;
+                    if (std.mem.containsAtLeast(u8, parent_dir, 1, delete)) continue;
+
+                    const joined_path = std.fs.path.join(allocator, &[_][]const u8{ path, parent_dir }) catch null;
+                    if (joined_path) |p| {
+                        defer allocator.free(p);
+
+                        const d = std.fs.cwd().openDir(p, .{}) catch continue;
+
+                        if (d.stat() catch null) |stat| {
+                            if (std.time.nanoTimestamp() - stat.mtime >= std.time.ns_per_week * 4) {
+                                const delete_path = std.fs.path.join(allocator, &[_][]const u8{ p, delete }) catch unreachable;
+                                defer allocator.free(delete_path);
+                                std.debug.print("More than 1 month has elapsed since modification: {s}\n DELETING\n", .{delete_path});
+                                std.fs.cwd().deleteTree(delete_path) catch {
+                                    std.debug.print("Failed to delete", .{});
+                                    continue;
+                                };
+                                std.debug.print("DELETED", .{});
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
